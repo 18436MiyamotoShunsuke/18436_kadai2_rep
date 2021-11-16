44da5520e8f5dc7dcd1e0af90e01571aeecebe2d
18436MiyamotoShunsuke <92012445+18436MiyamotoShunsuke@users.noreply.github.com>
2021-11-16 22:54:55 +0900

    aaa付け足した

--- tcp_client.py
+++ tcp_client.py
@@ -3,7 +3,7 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM)as s:
     #サーバを指定
     s.connect(('10.40.2.26',50007))
     #サーバにメッセージを送る
-    s.sendall(b'hello')
+    s.sendall(b'helloaaa')
     #ネットワークのバッファサイズは1024,サーバからの文字列を取得する
     data=s.recv(1024)
     print(repr(data))
